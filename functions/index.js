beaee21283cf3c653aa6e3fc73b56b5df64f72a1
KrokaKrola <harlamsan@gmail.com>
2020-01-07 21:19:57 +0300

    chat finish

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +12,21 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
+
+exports.onUserStatusChanged = functions.database
+  .ref("status/{userId}")
+  .onUpdate((change, context) => {
+    const eventStatus = change.after.val();
+    const userDoc = db.doc(`users/${context.params.userId}`);
+
+    return change.after.ref.once("value").then(snapshot => {
+      const status = snapshot.val();
+      if (status.lastChanged > eventStatus.lastChanged) {
+        return;
+      }
+      eventStatus.lastChanged = new Date(eventStatus.lastChanged);
+      userDoc.update({
+        status: eventStatus
+      });
+    });
+  });
